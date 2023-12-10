ecabc01f6d12fe25df37cc6152ab3be4f8f56c98
MariarosaMarchese <mariarosamarchese2@gmail.com>
2023-12-10 17:06:15 +0100

    Updated tabs

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { chatbox, square, triangle } from 'ionicons/icons';
+import { chatbox, calendarOutline, personOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({chatbox, square, triangle});
+    addIcons({chatbox, calendarOutline, personOutline});
   }
 }
