e1d811343e7ae4646bbbe618a919207757a9a696
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-12-13 19:23:07 +0100

    Added icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'granp.app.professional',
-  appName: 'granp-professional-app',
+  appName: 'GranP Professional',
   webDir: 'www',
   server: {
     androidScheme: 'https'
