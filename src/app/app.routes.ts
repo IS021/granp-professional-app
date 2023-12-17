5bdc6e7257a7ef19171530626f8bd0f94da162fb
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-12-17 16:56:38 +0100

    Fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,10 +20,5 @@ export const routes: Routes = [
     {
         path: 'modify-profile',
         loadComponent: () => import('./pages/modify-profile/modify-profile.page').then(m => m.ModifyProfilePage)
-    },
-  {
-    path: 'reservation-inbox',
-    loadComponent: () => import('./pages/reservation-inbox/reservation-inbox.page').then( m => m.ReservationInboxPage)
-  }
-
+    }
 ];
